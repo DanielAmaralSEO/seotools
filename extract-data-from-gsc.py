9ab00155fa0c6119b287396a094aedc8891ef28d
DanielAmaralSEO <seodanielamaral@gmail.com>
2025-12-10 14:43:37 -0300

    Update extract-data-from-gsc.py

--- extract-data-from-gsc.py
+++ extract-data-from-gsc.py
@@ -36,7 +36,7 @@ if "code" in st.query_params:
     flow = Flow.from_client_secrets_file(
         "client_secret.json",
         scopes=SCOPES,
-        redirect_uri=st.secrets["redirect_uri"]
+        redirect_uri=st.secrets["https://extract-data-from-gsc.streamlit.app/"]
     )
     
     flow.fetch_token(code=st.query_params["code"])
